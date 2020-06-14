becb09f918c8158c1e5d37452da72342bc35f03e
Dragonete <Dragonete@LAPTOP-BREVR659>
2020-06-14 16:27:08 +0200

    Estrategia de generacion de goles por equipos

--- LanzadorDadoMultiple/src/com/alcaudon/dado/vista/PanelJugarPartidoFutbol.java
+++ LanzadorDadoMultiple/src/com/alcaudon/dado/vista/PanelJugarPartidoFutbol.java
@@ -47,6 +47,9 @@ public class PanelJugarPartidoFutbol extends JPanel {
 	public JTextField texto;
 	//public String dadoPanel;
 	
+	JLabel marcadorLocal;
+	JLabel marcadorVisitante;
+	
 	public PanelJugarPartidoFutbol() {
 		
 		System.out.println("*** Cargamos Panel Principal ***");
@@ -82,20 +85,20 @@ public class PanelJugarPartidoFutbol extends JPanel {
 		// Añadimos un Marcador arriba
 		JPanel marcador = new JPanel();
 		
-		JTextField local = new JTextField("1");
-		JTextField visitante = new JTextField("0");
+		//JTextField local = new JTextField("1");
+		//JTextField visitante = new JTextField("0");
 		JLabel nombreLocal = new JLabel("Local", JLabel.CENTER);
 		JLabel nombreVisitante = new JLabel("Visitante", JLabel.CENTER);
-		JLabel marcadorLocal = new JLabel("1", JLabel.CENTER);
-		JLabel marcadorVisitante = new JLabel("0", JLabel.CENTER);
+		marcadorLocal = new JLabel("-", JLabel.CENTER);
+		marcadorVisitante = new JLabel("-", JLabel.CENTER);
 		
 		// Cambia el tamaño de la fuente a 20 
 		Font fuente = nombreLocal.getFont().deriveFont( 20f );
 		
 		nombreLocal.setFont(fuente);
 		nombreVisitante.setFont(fuente);
-		local.setFont(fuente);
-		visitante.setFont(fuente);
+		//local.setFont(fuente);
+		//visitante.setFont(fuente);
 		marcadorLocal.setFont(fuente);
 		marcadorVisitante.setFont(fuente);
 		
@@ -105,8 +108,8 @@ public class PanelJugarPartidoFutbol extends JPanel {
 		
 		nombreLocal.setPreferredSize(dimNombre);
 		nombreVisitante.setPreferredSize(dimNombre);
-		local.setPreferredSize(dimMarcador);
-		visitante.setPreferredSize(dimMarcador);
+		//local.setPreferredSize(dimMarcador);
+		//visitante.setPreferredSize(dimMarcador);
 		marcadorLocal.setPreferredSize(dimMarcador);
 		marcadorVisitante.setPreferredSize(dimMarcador);
 		
@@ -130,11 +133,15 @@ public class PanelJugarPartidoFutbol extends JPanel {
 	private class JugarPartidoFutbol implements ActionListener {
 
 		private static final int NUM_MINIMO = 1;
-		private static final int NUM_MAXIMO = 6;
+		private static final int NUM_MAXIMO = 100;
 		private static final int DELAY = 5000; // milisegundos
 		private static final int DADO_UNICO = 1;
 		private static final int DADO_DOBLE = 2;
 		private static final int DADO_MULTIPLE = 3;
+		
+		private static final int DADO_GOL = 85;
+		
+		private static final int RONDA_MINUTOS = 9;
 		
 		UtilidadesLanzadorDado utils;
 		
@@ -148,47 +155,170 @@ public class PanelJugarPartidoFutbol extends JPanel {
 			Integer cronometro = 0;
 			Integer dadoTotal = 0;
 			List<Integer> listaDados = new ArrayList<Integer>();
+			Integer dadoLocal = 0;
+			Integer dadoVisitante = 0;
+			Integer golesLocal = 0;
+			Integer golesVisitante = -1;
+			Integer intMarcadorLocal = 0;
+			Integer intMarcadorVisitante = 0;
 
 			// Obtenemos el boton
 			//String nombreBoton = (String) getValue(Action.NAME);
 
-			// Primer tiempo
+			System.out.println("---> Empieza el primer tiempo ");
+			
+			/**
+			 * ===========================================================================================================
+			 * Primer tiempo
+			 * ===========================================================================================================
+			 */
 			for (int c = 1; c <= 5; c++) {
-
+	
+				// Actualizamos cronometro en minutos
+				cronometro = c * RONDA_MINUTOS;
+				
+				// Lanzamos dado de Equipo Local
 				listaDados = servBasico.lanzaDado(NUM_MINIMO, NUM_MAXIMO, DELAY, DADO_UNICO);
 				
+				dadoTotal = 0;
 				for (Integer dadoItem : listaDados) {
 					dadoTotal+= dadoItem;
-					System.out.println ("dadoItem: " + dadoItem);
+					// System.out.println ("dadoItem: " + dadoItem);
 				}
 				
-				cronometro = c * 9;
+				dadoLocal = dadoTotal;
 				
-				System.out.println ("Cronometro: " + cronometro);
-				System.out.println ("Dado: " + dadoTotal);
+				// Gol local 
+				if (dadoLocal >= DADO_GOL) {
+					golesLocal += 1;
+					System.out.println ("*** Gooool ***");
+				}
+				
+				// Lanzamos dado de Equipo Visitante
+				listaDados = servBasico.lanzaDado(NUM_MINIMO, NUM_MAXIMO, DELAY, DADO_UNICO);
+				
+				dadoTotal = 0;
+				for (Integer dadoItem : listaDados) {
+					dadoTotal+= dadoItem;
+					//System.out.println ("dadoItem: " + dadoItem);
+				}
+				
+				dadoVisitante = dadoTotal;
+				
+				// Gol visitante 
+				if (dadoVisitante >= DADO_GOL) {
+					golesVisitante += 1;
+					System.out.println ("*** Gol ***");
+				}
+				
+				// Calculamos el Handicap
+				if (golesLocal < 0) {
+					intMarcadorVisitante = golesVisitante - golesLocal;
+					intMarcadorLocal = 0;
+				} else if (golesVisitante < 0) {
+					intMarcadorLocal = golesLocal - golesVisitante;
+					intMarcadorVisitante = 0;
+				} else {
+					intMarcadorLocal = golesLocal;
+					intMarcadorVisitante = golesVisitante;
+				}
+				
+				System.out.println ("*** Cronometro: minuto " + cronometro);
+				System.out.println ("Dado Local    : " + dadoLocal);
+				System.out.println ("Dado Visitante: " + dadoVisitante);
+				System.out.println ("Goles Local    : " + golesLocal);
+				System.out.println ("Goles Visitante: " + golesVisitante);
+				System.out.println ("==============================");
+				System.out.println ("Marcador Local    : " + intMarcadorLocal);
+				System.out.println ("Marcador Visitante: " + intMarcadorVisitante);
+				System.out.println ("==============================");
+				
+				// Actualizamos los Label del Panel 
+				marcadorLocal.setText(intMarcadorLocal.toString().trim());
+				marcadorVisitante.setText(intMarcadorVisitante.toString().trim());
 				
 			}
 
+			System.out.println("... Descanso ... ");
+			
 			// Intermedio
 			listaDados = servBasico.lanzaDado(NUM_MINIMO, NUM_MAXIMO, DELAY * 10, DADO_UNICO);
 			
-			// Segundo tiempo
+			System.out.println("---> Empieza el segundo tiempo ");
+			
+			/**
+			 * ===========================================================================================================
+			 * Segundo tiempo
+			 * ===========================================================================================================
+			 */
 			for (int c = 6; c <= 10; c++) {
 
+				// Actualizamos cronometro en minutos
+				cronometro = c * RONDA_MINUTOS;
+				
+				// Lanzamos dado de Equipo Local
 				listaDados = servBasico.lanzaDado(NUM_MINIMO, NUM_MAXIMO, DELAY, DADO_UNICO);
 				
+				dadoTotal = 0;
 				for (Integer dadoItem : listaDados) {
 					dadoTotal+= dadoItem;
-					System.out.println ("dadoItem: " + dadoItem);
+					//System.out.println ("dadoItem: " + dadoItem);
 				}
 				
-				cronometro = c * 9;
+				dadoLocal = dadoTotal;
 				
-				System.out.println ("Cronometro: " + cronometro);
-				System.out.println ("Dado: " + dadoTotal);
+				// Gol local 
+				if (dadoLocal >= DADO_GOL) {
+					golesLocal += 1;
+					System.out.println ("*** Gooool ***");
+				}
+				
+				// Lanzamos dado de Equipo Visitante
+				listaDados = servBasico.lanzaDado(NUM_MINIMO, NUM_MAXIMO, DELAY, DADO_UNICO);
+				
+				dadoTotal = 0;
+				for (Integer dadoItem : listaDados) {
+					dadoTotal+= dadoItem;
+					//System.out.println ("dadoItem: " + dadoItem);
+				}
+				
+				dadoVisitante = dadoTotal;
+				
+				// Gol visitante 
+				if (dadoVisitante >= DADO_GOL) {
+					golesVisitante += 1;
+					System.out.println ("*** Gol ***");
+				}
+				
+				// Calculamos el Handicap
+				if (golesLocal < 0) {
+					intMarcadorVisitante = golesVisitante - golesLocal;
+					intMarcadorLocal = 0;
+				} else if (golesVisitante < 0) {
+					intMarcadorLocal = golesLocal - golesVisitante;
+					intMarcadorVisitante = 0;
+				} else {
+					intMarcadorLocal = golesLocal;
+					intMarcadorVisitante = golesVisitante;
+				}
+				
+				System.out.println ("*** Cronometro: minuto " + cronometro);
+				System.out.println ("Dado Local    : " + dadoLocal);
+				System.out.println ("Dado Visitante: " + dadoVisitante);
+				System.out.println ("Goles Local    : " + golesLocal);
+				System.out.println ("Goles Visitante: " + golesVisitante);
+				System.out.println ("==============================");
+				System.out.println ("Marcador Local    : " + intMarcadorLocal);
+				System.out.println ("Marcador Visitante: " + intMarcadorVisitante);
+				System.out.println ("==============================");
+				
+				// Actualizamos los Label del Panel 
+				marcadorLocal.setText(intMarcadorLocal.toString().trim());
+				marcadorVisitante.setText(intMarcadorVisitante.toString().trim());
 				
 			}
 			
+			System.out.println("---> Final del partido ");
 			
 		}
 		
